231e0f5c76bbc75a5503961c1e2f56b98ef0d1e9
Hermes <henryhe@appstate.edu>
2024-10-22 16:00:38 -0400

    Started working on directedEdge

--- src/ui/edge.py
+++ src/ui/edge.py
@@ -1,6 +1,7 @@
-from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QPen
-from PyQt6.QtWidgets import QGraphicsLineItem
+import math
+from PyQt6.QtCore import Qt, QPointF
+from PyQt6.QtGui import QPen, QPolygonF, QBrush
+from PyQt6.QtWidgets import QGraphicsLineItem, QGraphicsPolygonItem
 
 
 class Edge(QGraphicsLineItem):
@@ -50,3 +51,60 @@ class Edge(QGraphicsLineItem):
         newY2 = self._linkVertex.y
         self.setLine(newX1, newY1, newX2, newY2)
         self._cosmeticLine.setLine(newX1, newY1, newX2, newY2)
+
+class DirectedEdge(Edge):
+    def __init__(self, originVertex, linkVertex):
+        super().__init__(originVertex, linkVertex)
+        
+        self._arrowHead = QGraphicsPolygonItem(self.getArrowPos())
+        self._arrowHead.setBrush(QBrush(Qt.GlobalColor.black))
+        self._arrowHead.setZValue(-2)
+    
+    def getArrowPos(self):
+        vertexRadius = 17.5
+        arrowSize = 15
+
+        linkPosX, linkPosY = (self._linkVertex.x, self._linkVertex.y)
+
+        # Establish our starting points
+        x1 = self._originVertex.x
+        x2 = linkPosX
+        y1 = self._originVertex.y
+        y2 = linkPosY
+
+        # Calculate the slope of the line intersecting our starting points
+        dy = (y2 - y1)
+        dx = (x2 - x1)
+        m = dy / dx if dx != 0 else 0
+        b = y1 - m * x1
+
+        # Calculate the intersection points of our line and a perpendicular line
+        m_inv = 1/m
+        x_int = (y2 + (m_inv) * x2 + arrowSize - b) / (m + (m_inv)) - vertexRadius
+        y_int = m * x_int + b
+
+        # Determine the two points equidistant from the intersection points
+        ux = 1 / math.sqrt(1 + (m_inv)**2)
+        uy = (-m_inv) / math.sqrt(1 + (m_inv)**2)
+
+        x1 = x_int + (arrowSize / 2) * ux
+        y1 = y_int + (arrowSize / 2) * uy
+
+        x2 = x_int - (arrowSize / 2) * ux
+        y2 = y_int - (arrowSize / 2) * uy
+
+        # Finally, create the two points making the corners of the arrow head
+        p1 = QPointF(x1, y1)
+        p2 = QPointF(x2, y2)
+        # And the tip of the arrow head
+        ux = 1 / math.sqrt(1 + m**2)
+        uy = m / math.sqrt(1 + m**2)
+        tipX = x_int + arrowSize * ux
+        tipY = y_int + arrowSize * uy
+        tip = QPointF(tipX, tipY)
+
+        return QPolygonF([tip, p1, p2])
+    
+    def updatePosition(self):
+        super().updatePosition()
+        self._arrowHead.setPolygon(self.getArrowPos())
