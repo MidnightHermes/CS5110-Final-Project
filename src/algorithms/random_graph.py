2389a0878628113bdb1fbeb7af197a95c8c271da
Hermes <henryhe@appstate.edu>
2024-10-30 14:39:19 -0400

    Minor changes to RandomGraph

--- src/algorithms/random_graph.py
+++ src/algorithms/random_graph.py
@@ -9,7 +9,7 @@ class RandomGraph:
     # TODO: Create different graph generation methods and pass type of graph as parameter
     def __init__(self, directed=False, seed=DEFAULT_SEED):
         self.graph = self.gen_random_complete_connected_weighted_graph(directed=directed, seed=seed)
-    
+
     def gen_random_complete_connected_weighted_graph(self, directed: bool, seed: int) -> nx.Graph | nx.DiGraph:
         # We assume a graph with less than 3 nodes is trivial so we ensure the seed is at least 3
         if seed < 3:
@@ -23,8 +23,8 @@ class RandomGraph:
         max_edges = num_nodes * (num_nodes - 1) // 2 if not directed else num_nodes * (num_nodes - 1)
         min_edges = num_nodes - 1
         num_edges = random.randint(min_edges, max_edges)
-        
+
         G = nx.gnm_random_graph(num_nodes, num_edges, directed=directed)
         for edge in G.edges():
-            G.edges[*edge]['weight'] = random.randint(1, 10)  # Assign random weights between 1 and 10
+            G.edges[*edge]['weight'] = random.randint(1, seed)  # Assign random weights between 1 and 10
         return G
