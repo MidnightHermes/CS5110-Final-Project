c220441f953ff1c100cc0abe6020eb20cceb5542
dsmurrow <dsmurrow@gmail.com>
2024-12-04 14:00:12 -0500

    Ramsey algorithm is correct now

--- src/algorithms/max_clique.py
+++ src/algorithms/max_clique.py
@@ -2,9 +2,12 @@ import networkx as nx
 import random
 
 def ramsey(g: nx.Graph):
-    if g.number_of_nodes == 0:
+    """
+    Respectively approximates the largest clique and independent set in a graph.
+    """
+    if g.number_of_nodes() == 0:
         return set(), set()
-    
+   
     v = random.choice(sorted(g.nodes))
 
     node_set = set(g.nodes)
@@ -13,11 +16,13 @@ def ramsey(g: nx.Graph):
     non_neighbors = node_set - neighbors
 
     neighbor_graph = g.copy()
+    neighbor_graph.remove_node(v)
     neighbor_graph.remove_nodes_from(non_neighbors)
 
     c1, i1 = ramsey(neighbor_graph)
 
     non_neighbor_graph = g.copy()
+    non_neighbor_graph.remove_node(v)
     non_neighbor_graph.remove_nodes_from(neighbors)
 
     c2, i2 = ramsey(non_neighbor_graph)
